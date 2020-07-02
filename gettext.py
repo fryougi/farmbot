545c9d5ced13a21d315171ec3e39f37deeaf8ff7
Mana Ryougi <fryougi@gmail.com>
2020-07-02 00:17:15 -0600

    Update gettext.py

--- gettext.py
+++ gettext.py
@@ -12,17 +12,22 @@ import pytesseract
 import time
 
 # Load image
-cvimage = cv2.imread('screencaps/frame0.png')
-tmpl = cv2.imread('templates/blue/repeatquest.png')
+cvimage = cv2.imread('screencaps/battle.png')
+cvframe = cvimage[8:40,854:924]
 
 # List of location windows
 # (xtol, ytol)
 # (xmin, ymin, xmax, ymax)
+cvframe = cv2.cvtColor(cvframe, cv2.COLOR_BGR2GRAY)
+cvframe = cv2.threshold(255-cvframe, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#cv2.imshow('cvimage', cvframe)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 # Compute match
-custom_config = r'--oem 3 --psm 6'
 time_start = time.perf_counter()
-print(pytesseract.image_to_string(tmpl, config=custom_config))
+wave = pytesseract.image_to_string(cvframe, config=r'-l eng --oem 3 --psm 7')
 time_end = time.perf_counter()
 seconds = time_end - time_start
-print("{:f}".format(seconds))
+print("{} {:f}".format(wave, seconds))
